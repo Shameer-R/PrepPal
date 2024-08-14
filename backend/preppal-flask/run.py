a011ebd31ff4836b36d0371900888c602b5e439c
Shameer-R <roseshammy@gmail.com>
2024-08-14 18:54:31 -0400

    Implement register functionality

--- backend/preppal-flask/run.py
+++ backend/preppal-flask/run.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_login import LoginManager, current_user
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
@@ -28,6 +28,32 @@ def home():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        # Check if user already exists
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user is None:
+
+            # Create new user
+            new_user = User(username=username, email=email)
+            new_user.set_password(password)
+
+            # Add the user to the database
+            db.session.add(new_user)
+            db.session.commit()
+
+            flash('Registration successful! Please log in.')
+            return redirect(url_for('login'))
+        else:
+            flash('Username/Email already exists!')
+            return redirect(url_for('register'))
+
+
+
+
     return  render_template('Register.html')
 
 @app.route('/login', methods=['GET', 'POST'])
